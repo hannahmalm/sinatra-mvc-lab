fb15cb303e72f0976dc6b1901cd6bf85d0d09f4b
Hannah Malm <hannahemalm616@gmail.com>
2021-08-22 11:59:59 -0400

    commit tree af62e645f72fb0a8755c034f05b5a4a16ac9c3c2

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    #this is a basic user input that is getting input from the views page
+    get '/' do 
+        erb :user_input
+    end 
+
+    #after you get the input, post it on this new route
+    #set piglatin as an instance variable
+    post '/piglatinizer' do 
+        pl= pigLatinizer.new 
+        @piglatin = pl.piglatinize(params[:user_phrase])
+        erb :results
+    end 
 end
+
+#Model is where the pig latin logic is held
+#Views are where the erbs are held that make the forms in HTML
+#app.rb is where the routes are found 
